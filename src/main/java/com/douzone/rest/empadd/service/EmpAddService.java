11e7c7efa531bc08e103dabd22d33059c487086d
seyeo <seoyeonev@gmail.com>
2023-08-17 16:24:31 +0900

    refacor. 구조 설정

--- src/main/java/com/douzone/rest/empadd/service/EmpAddService.java
+++ src/main/java/com/douzone/rest/empadd/service/EmpAddService.java
@@ -1,7 +1,30 @@
 package com.douzone.rest.empadd.service;
 
+import com.douzone.rest.empadd.dao.EmpAddDao;
+import com.douzone.rest.empadd.vo.EmpAdd;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+import java.util.List;
 
-
+@Service
 public class EmpAddService {
+
+    private EmpAddDao empAddDao;
+    @Autowired
+    public EmpAddService(EmpAddDao empAddDao) {
+        this.empAddDao = empAddDao;
+    }
+
+    public List<EmpAdd> getAllEmpAdd(){
+        List<EmpAdd> empAdds = null;
+        empAdds = empAddDao.getAllEmpAdd();
+        return empAdds;
+    }
+
+    public Integer insertEmpAdd(EmpAdd empAdd){
+        Integer result = null;
+        result = empAddDao.insertEmpAdd(empAdd);
+        return result;
+    }
 }
