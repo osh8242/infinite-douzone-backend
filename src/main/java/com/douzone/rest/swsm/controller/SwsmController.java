d1da516a6264f125963c33f177d45b393bd47164
seyeo <seoyeonev@gmail.com>
2023-08-17 17:05:32 +0900

    refactor. mapper 수정

--- src/main/java/com/douzone/rest/swsm/controller/SwsmController.java
+++ src/main/java/com/douzone/rest/swsm/controller/SwsmController.java
@@ -5,10 +5,7 @@ import com.douzone.rest.swsm.vo.Swsm;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -19,7 +16,6 @@ public class SwsmController {
     //   @Autowired
     //    private SwsmService swsmService;
 
-    @Autowired
     private SwsmService swsmService;
 
     @Autowired
@@ -33,6 +29,8 @@ public class SwsmController {
         return ResponseEntity.status(HttpStatus.OK).body(swsmList);
     }
 
+
+    @PostMapping("/insertSwsm")
     public ResponseEntity<Integer> insertSwsm(@RequestBody Swsm swsm){
         int result=swsmService.insertSwsm(swsm);
         return ResponseEntity.status(HttpStatus.OK).body(result);
