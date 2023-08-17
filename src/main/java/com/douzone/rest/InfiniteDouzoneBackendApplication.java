24ae515ee31ef9da33f31fec8d3b87b53a788a66
osh8242 <osh8242@gmail.com>
2023-08-17 13:33:11 +0900

    Initial commit

---
+++ src/main/java/com/douzone/rest/InfiniteDouzoneBackendApplication.java
@@ -0,0 +1,13 @@
+package com.douzone.rest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InfiniteDouzoneBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InfiniteDouzoneBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/douzone/rest/InfiniteDouzoneBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.douzone.rest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InfiniteDouzoneBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
