ba24d7749050299e968de060683a660db494c98c
osh <osh8242@gmail.com>
2023-08-30 23:23:05 +0900

    feat. EMP contoller 수정 및 insert 문 수정

--- src/main/java/com/douzone/rest/emp/controller/EmpController.java
+++ src/main/java/com/douzone/rest/emp/controller/EmpController.java
@@ -17,64 +17,67 @@ import java.util.Map;
 public class EmpController {
 
     private EmpService empservice;
+
     @Autowired
     public EmpController(EmpService empservice) {
         this.empservice = empservice;
     }
 
     @GetMapping("/getAll")
-    public ResponseEntity<List<Emp>> getAllEmp(){
+    public ResponseEntity<List<Emp>> getAllEmp() {
         System.out.println("Emp Controller 진입");
         List<Emp> result = empservice.getAllEmp();
-        System.out.println("result = "+result);
+        System.out.println("result = " + result);
         return new ResponseEntity<>(result, HttpStatus.OK);
     }
 
-    @PostMapping("/getEmpListByJobOk")
-    public ResponseEntity<List<Emp>> getEmpListByJobOk(@RequestBody Emp emp, @RequestParam("orderRef") String orderRef){
+    @GetMapping("/getEmpListByJobOk")
+    public ResponseEntity<List<Emp>> getEmpListByJobOk(@RequestParam(name = "jobOk", required = false) String jobOk,
+                                                       @RequestParam(name = "orderRef", required = false) String orderRef,
+                                                       @RequestParam(name = "refYear", required = false) String refYear) {
         System.out.println("EmpController.getEmpListByJobOk");
-        System.out.println("orderRef = " + orderRef);
         Map<String, Object> map = new HashMap<>();
-        map.put("emp", emp);
-        map.put("orderRef", orderRef);
+        map.put("jobOk", jobOk.trim());
+        map.put("refYear", refYear.trim());
+        map.put("orderRef", orderRef.trim());
         List<Emp> list = null;
         list = empservice.getEmpListByJobOk(map);
         return new ResponseEntity<>(list, HttpStatus.OK);
     }
 
     @PostMapping("/getOne")
-    public ResponseEntity<Emp> getOneEmpByCdEmp(@RequestBody Emp emp){
+    public ResponseEntity<Emp> getOneEmpByCdEmp(@RequestBody Emp emp) {
         System.out.println("Emp getOneEmpByCdEmp Controller -----");
         Emp resultEmp = empservice.getOneEmpByCdEmp(emp);
         return new ResponseEntity<>(resultEmp, HttpStatus.OK);
     }
 
-//    {columnName: 컬럼명, columnValue: 컬럼값} 으로 전달받아 검색하는 기능
+    //    {columnName: 컬럼명, columnValue: 컬럼값} 으로 전달받아 검색하는 기능
     @PostMapping("/getListByVariable")
-    public ResponseEntity<List<Emp>> getListByVariable(@RequestBody Map<String, String> variable){
+    public ResponseEntity<List<Emp>> getListByVariable(@RequestBody Map<String, String> variable) {
         System.out.println("---------- Emp getListByColumn Controller 시작 ----------");
         List<Emp> result = empservice.getListEmpByVariable(variable);
         return new ResponseEntity<>(result, HttpStatus.OK);
     }
 
     @PostMapping("insertEmp")
-    public int insertEmp(@RequestBody Emp emp){
+    public int insertEmp(@RequestBody Emp emp) {
         System.out.println("Emp insert Controller -----");
         int result = empservice.insertEmp(emp);
         return result;
     }
 
     @PutMapping("/updateEmp")
-    public int updateEmp(@RequestBody Emp emp){
+    public int updateEmp(@RequestBody Emp emp) {
         System.out.println("Emp update Controller -----");
         int result = empservice.updateEmp(emp);
         return result;
     }
 
     @PostMapping("/delete")
-    public int deleteEmp(@RequestBody Emp emp){
+    public int deleteEmp(@RequestBody Emp emp) {
         System.out.println("Emp delete Controller -----");
-        int result =empservice.deleteEmp(emp);
+        int result = empservice.deleteEmp(emp);
         return result;
     }
 }
