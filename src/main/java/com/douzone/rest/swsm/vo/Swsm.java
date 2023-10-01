10ad13f9cd92c47ef967ddd06ade1dd75d78548d
seyeo <seoyeonev@gmail.com>
2023-10-01 23:33:42 +0900

    feat: insertSwsm

--- src/main/java/com/douzone/rest/swsm/vo/Swsm.java
+++ src/main/java/com/douzone/rest/swsm/vo/Swsm.java
@@ -1,12 +1,8 @@
 package com.douzone.rest.swsm.vo;
 
 import lombok.Data;
-import lombok.Getter;
-import lombok.Setter;
 
 @Data
-@Getter
-@Setter
 public class Swsm {
     String withholdingYear; // "원천년도"
     String dateOfCreation; // "작성년월"
