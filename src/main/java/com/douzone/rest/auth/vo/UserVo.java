8db513ca67ecca6bb7a1aa569a4d834a8aad4d2a
osh <osh8242@gmail.com>
2023-10-06 16:40:57 +0900

    feat: companyName 추가

--- src/main/java/com/douzone/rest/auth/vo/UserVo.java
+++ src/main/java/com/douzone/rest/auth/vo/UserVo.java
@@ -11,6 +11,7 @@ import lombok.*;
 @NoArgsConstructor
 public class UserVo {
     private String companyCode;
+    private String companyName;
     private String userId;
     private String userPwd;
     private String userName;
