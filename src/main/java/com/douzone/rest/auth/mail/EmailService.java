3cfc5281bce83c954cdfd6d7478f40135f5ac8af
seyeo <seoyeonev@gmail.com>
2023-10-14 04:51:36 +0900

    feat: 근로계약 메일 송부

--- src/main/java/com/douzone/rest/auth/mail/EmailService.java
+++ src/main/java/com/douzone/rest/auth/mail/EmailService.java
@@ -3,13 +3,17 @@ package com.douzone.rest.auth.mail;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
+
+import javax.mail.MessagingException;
+import javax.mail.internet.MimeMessage;
 
 @Service
 public class EmailService {
 
     @Autowired
-    private JavaMailSender emailSender;
+        private JavaMailSender emailSender;
 
     public void sendSimpleMessage(String to, String subject, String text) {
         SimpleMailMessage message = new SimpleMailMessage();
@@ -17,6 +21,21 @@ public class EmailService {
         message.setSubject(subject);
         message.setText(text);
         emailSender.send(message);
+    }
+
+    public void sendSwsmTable(String to, String subject, String html){
+        try{
+            MimeMessage message =emailSender.createMimeMessage();
+            MimeMessageHelper helper = new MimeMessageHelper(message, true, "UTF-8"); // for multipart messages
+
+            helper.setTo(to);
+            helper.setSubject(subject);
+            helper.setText(html, true);
+
+            emailSender.send(message);
+        }catch (MessagingException e){
+            e.printStackTrace();
+        }
     }
 
 
